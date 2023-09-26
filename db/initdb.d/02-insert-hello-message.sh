5ae4739040f4f1b9a80e912aa8aa177ecff64130
Jason Grimes <jason@grimesit.com>
2023-09-26 18:31:58 -0400

    Tweak db init script

--- db/initdb.d/02-insert-hello-message.sh
+++ db/initdb.d/02-insert-hello-message.sh
@@ -6,5 +6,5 @@
 # Run by the build process when the data directory is empty.
 #
 
-NOW=$(date +%Y-%m-%dT%H:%M:%S)
-psql -U $POSTGRES_USER -d $POSTGRES_DB -c "INSERT INTO hello (message) VALUES ('Hello from db data (written ${NOW} by ${HOSTNAME})');"
+NOW=$(date +%Y-%m-%dT%H:%M:%SZ)
+psql -U $POSTGRES_USER -d $POSTGRES_DB -c "INSERT INTO hello (message) VALUES ('Hello from db data (${HOSTNAME} wrote at ${NOW})');"
